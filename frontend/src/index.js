995a71b545f7577138febaefc1d6c7a38efdea49
mattasaichand <99887336+mattasaichand@users.noreply.github.com>
2025-01-18 15:23:52 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css"; // Global styles
-import App from "./App.js"; // Main App component
+import "./index.css"; 
+import App from "./App.js"; 
 
-// Render the App component into the root DOM node
 ReactDOM.render(
   <React.StrictMode>
     <App />

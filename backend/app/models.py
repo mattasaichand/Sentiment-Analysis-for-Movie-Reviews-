65b6433e1fc736255cb740c8ea4e59d3b78ecef8
mattasaichand <99887336+mattasaichand@users.noreply.github.com>
2025-01-18 15:23:01 +0530

    Update models.py

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,9 +1,7 @@
 from pydantic import BaseModel
 
-# Request body schema for analyzing sentiment of the review
 class SentimentRequest(BaseModel):
     review: str
 
-# Response schema for sentiment analysis
 class SentimentResponse(BaseModel):
     sentiment: str
